da639fd5fa7856544ae7a5b022a6d960f393e577
Grabiel1212 <136027575+Grabiel1212@users.noreply.github.com>
2024-08-29 19:10:07 -0500

    trafercia de proyecto a Netbeans 22

---
+++ src/java/controllers/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author GRABIEL
+ */
+public class HomeController  extends ActionSupport{
+    
+    
+    @Override
+    public String execute() throws Exception {
+        return super.execute(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    
+}
